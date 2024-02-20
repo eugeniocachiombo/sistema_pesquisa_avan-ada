98871a6a1ad65baed25d38d69a59081cd9ee3df9
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-02-20 19:29:17 +0100

    Primeiro Commit

---
+++ database/MYSQL_script/script_sisterma_pesquisa.sql
@@ -0,0 +1,21 @@
+
+create database sistema_pesquisa;
+
+use sistema_pesquisa;
+
+create table cidadao_nacional (
+    id int primary key auto_increment not null,
+    nome varchar(60),
+    numero_bi int,
+    data_nascimento date,
+    naturalidade varchar(20),
+    nome_pai varchar(60),
+    nome_mae varchar(60),
+    data_emissao date,
+    data_validade date,
+    sexo enum('M', 'F'),
+    estado_civil varchar(20),
+    altura decimal(3,1),
+    residencia varchar(30),
+    provincia varchar(30)
+);

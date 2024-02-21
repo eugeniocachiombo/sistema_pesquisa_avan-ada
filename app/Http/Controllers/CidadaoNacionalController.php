3ad2747d2ec10bc5aac82968b2bdbcb7a2aff91d
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-02-21 22:15:39 +0100

    Codigo do método pesquisar pesquisarDadosPessoais melhorado

--- app/Http/Controllers/CidadaoNacionalController.php
+++ app/Http/Controllers/CidadaoNacionalController.php
@@ -7,15 +7,8 @@ use Illuminate\Support\Facades\DB;
 
 class CidadaoNacionalController extends Controller
 {
-    function registrarView() {
-        return view('ciadadao_nacional.registro');
-    }
-
-    function pesquisarView() {
-        return view('ciadadao_nacional.pesquisa.pesquisa');
-    }
-
-    function pesquisarDadosPessoais(Request $request) {
+    public function pesquisarDadosPessoais(Request $request)
+    {
         $input_nome = $request->input("nome");
         $input_numero_bi = $request->input("numero_bi");
         $input_naturalidade = $request->input("naturalidade");
@@ -26,14 +19,17 @@ class CidadaoNacionalController extends Controller
         $input_altura = $request->input("altura");
         $input_residencia = $request->input("residencia");
         $input_provincia = $request->input("provincia");
+
         $input_mes_emissao_inicial = $request->input("mes_emissao_inicial");
         $input_ano_emissao_inicial = $request->input("ano_emissao_inicial");
         $input_mes_emissao_terminal = $request->input("mes_emissao_terminal");
         $input_ano_emissao_terminal = $request->input("ano_emissao_terminal");
+
         $input_mes_validade_inicial = $request->input("mes_validade_inicial");
         $input_ano_validade_inicial = $request->input("ano_validade_inicial");
         $input_mes_validade_terminal = $request->input("mes_validade_terminal");
         $input_ano_validade_terminal = $request->input("ano_validade_terminal");
+
         $input_mes_nascimento_inicial = $request->input("mes_nascimento_inicial");
         $input_ano_nascimento_inicial = $request->input("ano_nascimento_inicial");
         $input_mes_nascimento_terminal = $request->input("mes_nascimento_terminal");
@@ -66,95 +62,118 @@ class CidadaoNacionalController extends Controller
         $ano_nascimento_terminal = $input_ano_nascimento_terminal ? $input_ano_nascimento_terminal : "";
 
         $condicao = [];
-
-        if (!empty($nome)) {
-            $condicao[] = "nome = '{$nome}'";
-        }
-
-        if (!empty($numero_bi)) {
-            $condicao[] = "numero_bi = '{$numero_bi}'";
-        }
-
-        if (!empty($naturalidade)) {
-            $condicao[] = "naturalidade = '{$naturalidade}'";
-        }
-
-        if (!empty($nome_pai)) {
-            $condicao[] = "nome_pai = '{$nome_pai}'";
-        }
-
-        if (!empty($nome_mae)) {
-            $condicao[] = "nome_mae = '{$nome_mae}'";
-        }
-
-        if (!empty($sexo)) {
-            $condicao[] = "sexo = '{$sexo}'";
-        }
-
-        if (!empty($estado_civil)) {
-            $condicao[] = "estado_civil = '{$estado_civil}'";
-        }
-
-        if (!empty($altura)) {
-            $condicao[] = "altura = '{$altura}'";
-        }
-
-        if (!empty($residencia)) {
-            $condicao[] = "residencia = '{$residencia}'";
-        }
-
-        if (!empty($provincia)) {
-            $condicao[] = "provincia = '{$provincia}'";
-        }
-
-        if (!empty($mes_emissao_terminal) && !empty($ano_emissao_terminal) && !empty($mes_emissao_inicial) && !empty($ano_emissao_inicial)) {
-            
-            $emissao_inicial_tratado = $ano_emissao_inicial . "-" . $mes_emissao_inicial . "-01";
-            
-            $emissao_terminal_tratado = $ano_emissao_terminal . "-" . $mes_emissao_terminal . "-31";
-            
-            $condicao[] = "data_emissao between '{$emissao_inicial_tratado}' and '{$emissao_terminal_tratado}' ";
-            
-        } else if (!empty($mes_emissao_inicial) && !empty($ano_emissao_inicial)) {
-            $condicao[] = "month(data_emissao) = '{$mes_emissao_inicial}' and year(data_emissao) = '{$ano_emissao_inicial}'";
-        }
-
-        if (!empty($mes_validade_terminal) && !empty($ano_validade_terminal) && !empty($mes_validade_inicial) && !empty($ano_validade_inicial)) {
-            
-            $validade_inicial_tratado = $ano_validade_inicial . "-" . $mes_validade_inicial . "-01";
-            
-            $validade_terminal_tratado = $ano_validade_terminal . "-" . $mes_validade_terminal . "-31";
-            
-            $condicao[] = "data_validade between '{$validade_inicial_tratado}' and '{$validade_terminal_tratado}' ";
-            
-        } else if (!empty($mes_validade_inicial) && !empty($ano_validade_inicial)) {
-            $condicao[] = "month(data_validade) = '{$mes_validade_inicial}' and year(data_validade) = '{$ano_validade_inicial}'";
-        }
-
-        if (!empty($mes_nascimento_terminal) && !empty($ano_nascimento_terminal) && !empty($mes_nascimento_inicial) && !empty($ano_nascimento_inicial)) {
-            
-            $nascimento_inicial_tratado = $ano_nascimento_inicial . "-" . $mes_nascimento_inicial . "-01";
-            
-            $nascimento_terminal_tratado = $ano_nascimento_terminal . "-" . $mes_nascimento_terminal . "-31";
-            
-            $condicao[] = "data_nascimento between '{$nascimento_inicial_tratado}' and '{$nascimento_terminal_tratado}' ";
-            
-        } else if (!empty($mes_nascimento_inicial) && !empty($ano_nascimento_inicial)) {
-            $condicao[] = "month(data_nascimento) = '{$mes_nascimento_inicial}' and year(data_nascimento) = '{$ano_nascimento_inicial}'";
-        }
-
-        if (!empty($condicao)) {
-        $condicao_final = implode(" and ", $condicao);
-        $condicao_final = str_replace("Array", "", $condicao_final);
-
-        $query = DB::table('cidadao_nacional')
-                    ->whereRaw($condicao_final)
-                    ->paginate(5);
-        $resultado = $query;
-        return view('ciadadao_nacional.pesquisa.pesquisa', compact("resultado"));
-            
+        $componentes = [
+            "nome" => $nome,
+            "numero_bi" => $numero_bi,
+            "naturalidade" => $naturalidade,
+            "nome_pai" => $nome_pai,
+            "nome_mae" => $nome_mae,
+            "sexo" => $sexo,
+            "estado_civil" => $estado_civil,
+            "altura" => $altura,
+            "residencia" => $residencia,
+            "provincia" => $provincia,
+            "mes_emissao_inicial" => $mes_emissao_inicial,
+            "ano_emissao_inicial" => $ano_emissao_inicial,
+            "mes_emissao_terminal" => $mes_emissao_terminal,
+            "ano_emissao_terminal" => $ano_emissao_terminal,
+            "mes_validade_inicial" => $mes_validade_inicial,
+            "ano_validade_inicial" => $ano_validade_inicial,
+            "mes_validade_terminal" => $mes_validade_terminal,
+            "ano_validade_terminal" => $ano_validade_terminal,
+            "mes_nascimento_inicial" => $mes_nascimento_inicial,
+            "ano_nascimento_inicial" => $ano_nascimento_inicial,
+            "mes_nascimento_terminal" => $mes_nascimento_terminal,
+            "ano_nascimento_terminal" => $ano_nascimento_terminal,
+        ];
+        $nova_condicao = $this->verficarComponentesComValores($componentes, $condicao);
+        $resultado = $this->verficarCondicao($nova_condicao);
+        if($resultado){
+            return view('ciadadao_nacional.pesquisa.pesquisa', compact("resultado"));
         }else{
-            echo "Nenhuma opção selecionada";
+            return view('ciadadao_nacional.pesquisa.pesquisa');
         }
     }
+
+    public function verficarComponentesComValores($componentes, $condicao)
+    {
+        if (!empty($componentes['nome'])) {
+            $condicao[] = "nome = '{$componentes['nome']}'";
+        }
+
+        if (!empty($componentes['numero_bi'])) {
+            $condicao[] = "numero_bi = '{$componentes['numero_bi']}'";
+        }
+
+        if (!empty($componentes['naturalidade'])) {
+            $condicao[] = "naturalidade = '{$componentes['naturalidade']}'";
+        }
+
+        if (!empty($componentes['nome_pai'])) {
+            $condicao[] = "nome_pai = '{$componentes['nome_pai']}'";
+        }
+
+        if (!empty($componentes['nome_mae'])) {
+            $condicao[] = "nome_mae = '{$componentes['nome_mae']}'";
+        }
+
+        if (!empty($componentes['sexo'])) {
+            $condicao[] = "sexo = '{$componentes['sexo']}'";
+        }
+
+        if (!empty($componentes['estado_civil'])) {
+            $condicao[] = "estado_civil = '{$componentes['estado_civil']}'";
+        }
+
+        if (!empty($componentes['altura'])) {
+            $condicao[] = "altura = '{$componentes['altura']}'";
+        }
+
+        if (!empty($componentes['residencia'])) {
+            $condicao[] = "residencia = '{$componentes['residencia']}'";
+        }
+
+        if (!empty($componentes['provincia'])) {
+            $condicao[] = "provincia = '{$componentes['provincia']}'";
+        }
+
+        if (!empty($componentes['mes_nascimento_terminal']) && !empty($componentes['ano_nascimento_terminal']) && !empty($componentes['mes_nascimento_inicial']) && !empty($componentes['ano_nascimento_inicial'])) {
+            $nascimento_inicial_tratado = $componentes['ano_nascimento_inicial'] . "-" . $componentes['mes_nascimento_inicial'] . "-01";
+            $nascimento_terminal_tratado = $componentes['ano_nascimento_terminal']  . "-" . $componentes['mes_nascimento_terminal'] . "-31";
+            $condicao[] = "data_nascimento between '{$nascimento_inicial_tratado}' and '{$nascimento_terminal_tratado}' ";
+        } else if (!empty($componentes['mes_nascimento_inicial']) && !empty($componentes['ano_nascimento_inicial'])) {
+            $condicao[] = "month(data_nascimento) = '{$componentes['mes_nascimento_inicial']}' and year(data_nascimento) = '{$componentes['ano_nascimento_inicial']}'";
+        }
+
+        if (!empty($componentes['mes_emissao_terminal']) && !empty($componentes['ano_emissao_terminal']) && !empty($componentes['mes_emissao_inicial']) && !empty($componentes['ano_emissao_inicial'])) {
+            $emissao_inicial_tratado = $componentes['ano_emissao_inicial'] . "-" . $componentes['mes_emissao_inicial'] . "-01";
+            $emissao_terminal_tratado = $componentes['ano_emissao_terminal']  . "-" . $componentes['mes_emissao_terminal'] . "-31";
+            $condicao[] = "data_emissao between '{$emissao_inicial_tratado}' and '{$emissao_terminal_tratado}' ";
+        } else if (!empty($componentes['mes_emissao_inicial']) && !empty($componentes['ano_emissao_inicial'])) {
+            $condicao[] = "month(data_emissao) = '{$componentes['mes_emissao_inicial']}' and year(data_emissao) = '{$componentes['ano_emissao_inicial']}'";
+        }
+
+        if (!empty($componentes['mes_validade_terminal']) && !empty($componentes['ano_validade_terminal']) && !empty($componentes['mes_validade_inicial']) && !empty($componentes['ano_validade_inicial'])) {
+            $validade_inicial_tratado = $componentes['ano_validade_inicial'] . "-" . $componentes['mes_validade_inicial'] . "-01";
+            $validade_terminal_tratado = $componentes['ano_validade_terminal']  . "-" . $componentes['mes_validade_terminal'] . "-31";
+            $condicao[] = "data_validade between '{$validade_inicial_tratado}' and '{$validade_terminal_tratado}' ";
+        } else if (!empty($componentes['mes_validade_inicial']) && !empty($componentes['ano_validade_inicial'])) {
+            $condicao[] = "month(data_validade) = '{$componentes['mes_validade_inicial']}' and year(data_validade) = '{$componentes['ano_validade_inicial']}'";
+        }
+
+        return $condicao;
+    }
+
+    public function verficarCondicao($condicao){
+        if (!empty($condicao)) {
+            $condicao_final = implode(" and ", $condicao);
+            $condicao_final = str_replace("Array", "", $condicao_final);
+
+            $query = DB::table('cidadao_nacional')
+                ->whereRaw($condicao_final)
+                ->paginate(5);
+            return $query;
+        } 
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CidadaoNacionalController;
 
-
 Route::get('/', [CidadaoNacionalController::class, 'pesquisarView']);
 Route::get('/ciadadao_nacional/pesquisa', [CidadaoNacionalController::class, 'pesquisarView']);
 Route::post('/cidadao_nacional/pesquisa/dados_pessoais', [CidadaoNacionalController::class, 'pesquisarDadosPessoais']);
